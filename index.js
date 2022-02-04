56d2f5df92cde47204cd8fff25d641e3ff0c7106
Kim_Origin <98868306+origin2008@users.noreply.github.com>
2022-02-04 18:02:13 +0900

    First upload
    null

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get(/p/, (req, res) => {
+res.writeHead(404, {'Content-Type': 'text/html'});
+res.write('<script>var prompt = prompt("^^", "")</script>');
+res.write(`<script>
+	document.write('<h1>'+prompt+'</h1>');
+	</script>`);
+res.end();
+});
+
+app.listen(3000, () => {
+	console.log('Hello World!!');
+	});
